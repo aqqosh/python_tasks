7cfa935cbe49d3d2f25db410868599ab003f9815
Elizaveta Koroleva <the.white.nurse@gmail.com>
2021-11-22 11:01:48 +0300

    fabric method alphabets

--- 01_fabric_method.py
+++ 01_fabric_method.py
@@ -1,76 +1,40 @@
-"""
-Классу заранее неизвестно, объекты каких подклассов ему нужно создать.
-Обязанности делегируются подклассу, а знания о том, какой подкласс принимает эти обязанности, локализованы.
-Создаваемые объекты родительского класса специализируются подклассами.
-"""
-
-"""
-Продукт 
-Определяет общий интерфейс объектов, которые может 
-произвести создатель и его подклассы.
-"""
-class Culture:
-    """Description"""
-    def __repr__(self):
+class Alphabet:
+    def __repr_(self):
         return self.__str__()
 
-"""
-Конкретные продукты 
-Cодержат код различных продуктов. Продукты будут отличаться 
-реализацией, но интерфейс у них будет общий.
-"""
+class EnglishAlphabet(Alphabet):
+    def __str__(self) -> str:
+        return "English alphabet"
 
-class Democracy(Culture):
+class GeorgianAlphabet(Alphabet):
+    def __str__(self) -> str:
+        return "Georgian alphabet"
+
+class RussianAlphabet(Alphabet):
+    def __str__(self) -> str:
+        return "Russian alphabet"
+
+class Language:
+    alphabet = ""
     def __str__(self):
-        return "Democracy"
-
-class Dictatiorship(Culture):
-    def __str__(self):
-        return "Dictatorship"
-
-"""
-Создатель
-Объявляет фабричный метод, который должен возвращать новые объекты продуктов. 
-Важно, чтобы тип результата совпадал с общим интерфейсом продуктов.
-Зачастую фабричный метод объявляют абстрактным, чтобы заставить все подклассы реализовать 
-его по-своему. Но он может возвращать и некий стандартный продукт.
-"""
-
-class Government:
-    """Description"""
-    culture = ""
-    def __str__(self):
-        return self.culture.__str__()
-
+        return self.alphabet.__str__()
     def __repr__(self):
-        return self.culture.__repr__()
+        return self.alphabet.__repr__()
+    def set_alphabet(self):
+        raise AttributeError("No Implemented Alphabet")
 
-    def set_culture(self):
-        raise AttributeError("No Implemented Culture")
+class EnglishLanguage(Language):
+    def set_alphabet(self):
+        self.alphabet = EnglishAlphabet()
 
-"""
-Конкретные создатели
-По-своему реализуют фабричный метод, производя те или иные конкретные продукты.
-Фабричный метод не обязан всё время создавать новые объекты. Его можно переписать 
-так, чтобы возвращать существующие объекты из какого-то хранилища или кэша.
-"""
+class GeorgianAlphabet(Language):
+    def set_alphabet(self):
+        self.alphabet = GeorgianAlphabet()
 
-class GovernmentA(Government):
-    def set_culture(self):
-        self.culture = Democracy()
+class RussianAlphabet(Language):
+    def set_alphabet(self):
+        self.alphabet = RussianAlphabet()
 
-class GovernmentB(Government):
-    def set_culture(self):
-        self.culture = Dictatiorship()
-
-"""
-Пример
-"""
-
-g1 = GovernmentA()
-g1.set_culture()
-print(str(g1))
-
-g2 = GovernmentB()
-g2.set_culture()
-print(str(g2))
+l1 = EnglishLanguage()
+l1.set_alphabet()
+print(str(l1))
